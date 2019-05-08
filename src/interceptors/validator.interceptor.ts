b10da01c87fe3b0d9f047ab00e9559a7e104f183
Daniel Cavalcante <c0500272@vale.com>
2019-05-08 09:29:38 -0300

    Fix interceptor

--- src/interceptors/validator.interceptor.ts
+++ src/interceptors/validator.interceptor.ts
@@ -1,20 +1,23 @@
 'use strinct'
 
-import { NestInterceptor, Injectable, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException, HttpStatus } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Contract } from 'src/backoffice/contracts/contract';
+import { Result } from 'src/backoffice/models/result.model';
 
 @Injectable()
 export class ValidatorInterceptor implements NestInterceptor {
+
   constructor(public contract: Contract) {}
 
-  intercept(context: ExecutionContext, call$: Observable<any>) : Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
     const body = context.switchToHttp().getRequest().body
     const valid = this.contract.validate(body)
 
     if (!valid)
-      throw new HttpException(new Result('Error', false, null, this.contract.errors, HttpStatus.BAD_REQUEST))
-    return call$
+      throw new HttpException(new Result('Error', false, null, HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST)
+
+    return next.handle()
   }
   
 }
