d26124ca8f0aa34241904ca0b074d866bb9593f6
Daniel Cavalcante <c0500272@vale.com>
2019-05-08 00:45:57 -0300

    Add Jarvis

---
+++ src/backoffice/contracts/customer.contracts.ts
@@ -0,0 +1,22 @@
+'use strinc'
+import { Jarvis } from 'src/utils/jarvis';
+import { Contract } from './contract';
+import { Customer } from '../models/customer.model';
+
+export class CreateCustomerContract implements Contract {
+
+  errors: any[];  
+  
+  validate(model: Customer): boolean {
+    const jarvis = new Jarvis();
+
+    jarvis.hasMinLen(model.name, 5, 'Invalid name')
+    jarvis.isEmail(model.email, 'Invalid email')
+    jarvis.isFixedLen(model.document, 11, 'Invalid CPF')
+    jarvis.hasMinLen(model.password, 6, 'Invalid password')
+    this.errors = jarvis.errors
+
+    return jarvis.isValid()
+  }
+
+}
