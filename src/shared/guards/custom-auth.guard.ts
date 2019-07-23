9bd4d21504c6757a964f58828ef7de5489e06231
Daniel Cavalcante <c0500272@vale.com>
2019-07-22 23:59:42 -0300

    Add jwt payload

---
+++ src/shared/guards/custom-auth.guard.ts
@@ -0,0 +1,18 @@
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard() {
+
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context)
+  }
+
+  handleRequest(error, user, info) {
+    if (error || !user) {
+      throw error || new UnauthorizedException()
+    }
+    return user
+  }
+
+}
